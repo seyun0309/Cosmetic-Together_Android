df1681f8699e9dbbd6e0d792e52118ffdf4e85ec
seyun kim <>
2024-11-06 18:41:13 +0900

    FEAT : 이미지뷰 데이터바인딩 함수

---
+++ app/src/main/java/com/example/cosmetictogether/presentation/mypage/viewmodel/BindingAdapters.kt
@@ -0,0 +1,15 @@
+package com.example.cosmetictogether.presentation.mypage.viewmodel
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("imageUrl")
+    fun loadImage(view: ImageView, imageUrl: String?) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .into(view)
+    }
+}
