43cc02a975351b72a84fabdf95dc631dcf829fc3
seyun kim <>
2024-11-13 10:14:05 +0900

    FEAT : uri 바인딩 설정

--- app/src/main/java/com/example/cosmetictogether/presentation/form/adapter/BindingAdapters.kt
+++ app/src/main/java/com/example/cosmetictogether/presentation/form/adapter/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.cosmetictogether.presentation.form.adapter
 
+import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -11,5 +12,15 @@ object BindingAdapters {
         Glide.with(view.context)
             .load(imageUrl)
             .into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("imageUri")
+    fun loadImage(view: ImageView, imageUri: Uri?) {
+        if (imageUri != null) {
+            Glide.with(view.context)
+                .load(imageUri.toString())
+                .into(view)
+        }
     }
 }
