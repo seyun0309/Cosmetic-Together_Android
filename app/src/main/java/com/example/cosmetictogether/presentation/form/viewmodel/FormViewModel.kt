e932234aebb6c84cc50ce18942cd7a0ba153051d
seyun kim <>
2025-06-07 21:22:59 +0900

    FEAT : 팔로잉 폼 조회 로직, 주문 후 판매자 계좌 내역 띄우는 로직 추가

--- app/src/main/java/com/example/cosmetictogether/presentation/form/viewmodel/FormViewModel.kt
+++ app/src/main/java/com/example/cosmetictogether/presentation/form/viewmodel/FormViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.cosmetictogether.data.api.FormRetrofitInterface
 import com.example.cosmetictogether.data.api.RetrofitClient
+import com.example.cosmetictogether.data.model.APIResponse
+import com.example.cosmetictogether.data.model.AccountResponse
 import com.example.cosmetictogether.data.model.FormSummaryResponse
 import retrofit2.Call
 import retrofit2.Callback
@@ -35,6 +37,11 @@ class FormViewModel: ViewModel() {
     private val _formData = MutableLiveData<List<FormSummaryResponse>>()
     val formData: LiveData<List<FormSummaryResponse>> get() = _formData
 
+    private val _accountData = MutableLiveData<AccountResponse>()
+    val accountData: LiveData<AccountResponse> get() = _accountData
+
+    private val _orderResponse = MutableLiveData<APIResponse>()
+    val orderResponse: LiveData<APIResponse> get() = _orderResponse
 
     private val formApi: FormRetrofitInterface =
         RetrofitClient.getInstance().create(FormRetrofitInterface::class.java)
@@ -63,6 +70,49 @@ class FormViewModel: ViewModel() {
             override fun onFailure(call: Call<List<FormSummaryResponse>>, t: Throwable) {
                 // 실패 시 처리 로직 (예: 로그 출력)
                 Log.e("LoadFormSummaryData", "Error: ${t.message}")
+            }
+        })
+    }
+
+    fun loadFollowingForm(token: String) {
+        formApi.getFollowingForm(token).enqueue(object : Callback<List<FormSummaryResponse>> {
+            override fun onResponse(call: Call<List<FormSummaryResponse>>, response: Response<List<FormSummaryResponse>>) {
+                if (response.isSuccessful) {
+                    response.body()?.let { data ->
+                        if (data.isNotEmpty()) {
+                            // _formData에 데이터 목록 설정
+                            _formData.value = data
+
+                            // 첫 번째 항목을 _formItem에 설정 (필요한 경우)
+                            _formItem.value = data[0]
+                            _thumbnail.value = data[0].thumbnail
+                            _title.value = data[0].title
+                            _organizer_url.value = data[0].organizer_url
+                            _formStatus.value = data[0].formStatus
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<FormSummaryResponse>>, t: Throwable) {
+                // 실패 시 처리 로직 (예: 로그 출력)
+                Log.e("LoadFormSummaryData", "Error: ${t.message}")
+            }
+        })
+    }
+
+    fun showAccount(orderId: Long) {
+        formApi.getAccount(orderId).enqueue(object : Callback<AccountResponse> {
+            override fun onResponse(call: Call<AccountResponse>, response: Response<AccountResponse>) {
+                if (response.isSuccessful) {
+                    _accountData.value = response.body()
+                } else {
+                    Log.e("FormViewModel", "API 응답 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<AccountResponse>, t: Throwable) {
+                Log.e("FormViewModel", "API 호출 실패: ${t.message}")
             }
         })
     }
