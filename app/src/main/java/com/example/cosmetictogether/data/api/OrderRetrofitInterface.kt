ee8e75b6dac1f2d8e3de338b0c28d7b0e6c47631
seyun kim <>
2025-06-07 21:28:28 +0900

    FEAT : 주문 상태 변경 API 추가

--- app/src/main/java/com/example/cosmetictogether/data/api/OrderRetrofitInterface.kt
+++ app/src/main/java/com/example/cosmetictogether/data/api/OrderRetrofitInterface.kt
@@ -1,11 +1,15 @@
 package com.example.cosmetictogether.data.api
 
+import com.example.cosmetictogether.data.model.APIResponse
 import com.example.cosmetictogether.data.model.OrderDetailResponse
 import com.example.cosmetictogether.data.model.OrderFormResponse
 import com.example.cosmetictogether.data.model.OrderListResponse
+import com.example.cosmetictogether.data.model.UpdateOrderStatusRequest
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface OrderRetrofitInterface {
@@ -21,4 +25,10 @@ interface OrderRetrofitInterface {
 
     @GET("/api/v1/order/my-form/{formId}")
     fun getOrderList(@Path("formId") formId: Long): Call<OrderListResponse>
+
+    @POST("/api/v1/order/status/{orderId}")
+    fun updateOrderStatus(
+        @Path("orderId") orderId: Long,
+        @Body updateOrderStatusRequest: UpdateOrderStatusRequest
+    ): Call<APIResponse>
 }
